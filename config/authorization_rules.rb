91070b287d8776d731bbb3b4d51fb88ae64da008
Gustavo Muller <gmuller@bse.com.uy>
2011-08-05 10:58:13 -0300

    roles no andan

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -1,6 +1,12 @@
 authorization do
-  role :admin do
-    has_permission_on [:levels], :to => [:index, :show]
-    has_permission_on [:employees], :to => [:index, :show, :edit, :new, :create, :update, :destroye]
+    role :admin do
+    has_permission_on [:levels], :to => [:index, :show, :edit, :new, :create, :update, :destroy]
+    has_permission_on [:employees], :to => [:index, :show, :edit, :new, :create, :update, :destroy]
+    has_permission_on [:root], :to => [:index, :show, :edit, :new, :create, :update, :destroy]
+    has_permission_on [:home], :to => [:index, :show, :edit, :new, :create, :update, :destroy]
+    has_permission_on [:user], :to => [:index, :show, :edit, :new, :create, :update, :destroy]
+    has_permission_on [:user_sessions], :to => [:index, :show, :edit, :new, :create, :update, :destroy]
+    has_permission_on [:skills], :to => [:index, :show, :edit, :new, :create, :update, :destroy]
+    has_permission_on [:skill_types], :to => [:index, :show, :edit, :new, :create, :update, :destroy]
   end
 end
