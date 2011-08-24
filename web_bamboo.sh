7c956d07719bdea3345f9cfa779974c907c461c4
Sebas <sebastian.sassi@moove-it.com>
2011-08-24 18:56:41 -0300

    bamboo config

--- web_bamboo.sh
+++ web_bamboo.sh
@@ -1,3 +1,9 @@
 #! /bin/sh
+mkdir tmp
+cp /home/ubuntu/bamboo_extra/BSE/metric_fu/ tmp/ -R
+
 rvm ruby-1.9.2@bse_taller exec bundle install
-rvm ruby-1.9.2@bse_taller rake -f Rakefile_bamboo RAILS_ENV=test bamboo --trace
+rvm ruby-1.9.2@bse_taller exec rake -f Rakefile_bamboo RAILS_ENV=test bamboo --trace
+
+cp tmp/metric_fu/ /home/ubuntu/bamboo_extra/BSE/ -R
+
