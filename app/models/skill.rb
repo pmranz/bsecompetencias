dbc314f23ec4cf0bd815bc5715a9100e054c133d
rails <rails@rails-VirtualBox.(none)>
2011-08-26 16:35:43 -0300

    ...

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -5,7 +5,11 @@ class Skill < ActiveRecord::Base
 
   validates :name, :description, :skill_type_id, :presence => true
 
-  accepts_nested_attributes_for :behaviors, :allow_destroy => :true
+  accepts_nested_attributes_for :behaviors, 
+   :reject_if => :all_blank,
+   :allow_destroy => :true
   #, :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }
+  
+  
 
 end
